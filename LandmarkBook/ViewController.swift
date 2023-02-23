7e838ba86f1e12ceff2aa2cdda61430f521c9ebb
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-23 13:25:04 +0300

    usage of tableview

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.textLabel?.text = "test"
+        var content = cell.defaultContentConfiguration()
+        content.text = "TableView"
+        content.secondaryText = "test"
+        cell.contentConfiguration = content
+        return cell
     }
 
 
